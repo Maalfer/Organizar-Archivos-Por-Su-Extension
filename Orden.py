7bcd855bbcdd0ef59722028a706618a40b98cf64
Desmon <61366533+desmonHak@users.noreply.github.com>
2022-12-11 22:03:34 +0100

    Ultima actualizacion

--- Orden.py
+++ Orden.py
@@ -1,5 +1,5 @@
 from os import mkdir, listdir
-from os.path import exists
+from os.path import exists, isfile
 from shutil import move, Error
 from argparse import ArgumentParser
 try:
@@ -83,7 +83,7 @@ def crear_carpetas_si_no_existe(carpetas, ruta="./"):
     _COLOR = COLOR()
     BannerInformativo = "{}[{}*{}] {}".format(_COLOR.LIGHTGREEN_EX, _COLOR.LIGHTRED_EX, _COLOR.LIGHTGREEN_EX, _COLOR.LIGHTYELLOW_EX)
     for carpeta in carpetas:
-        if exists(carpeta) != True:
+        if exists(ruta+carpeta) != True:
             # si la carpeta no existe exists() retorna False lo cual es diferente a True
             #  y esta condicion se llevara a cabo
             mkdir(ruta+carpeta)
@@ -133,14 +133,16 @@ def asociar_carpetas_con_extensiones(nombre_carpeta, extensiones_de_archivos, ru
     ArchivosParaEstaCarpeta = []
     
     for archivo in listdir(ruta):
-        if archivo.endswith(extensiones_de_archivos) == True and archivo not in ArchivosExcepciones:
+        if archivo.endswith(extensiones_de_archivos) == True and archivo not in ArchivosExcepciones and isfile(ruta+archivo):
             """"
                 "archivo not in ArchivosExcepciones" comprueba que nuestro archivo no esta
                 en la lista de ArchivosExcepciones, en caso de que lo este, este archivo no
                 se agrega a la lista de ArchivosParaEstaCarpeta, ya que no queremos mover los
                 archivos que se contiene en ArchivosExcepciones.
+                isfile() comprueba que sea un archivo y un carpeta, pues si hay una carpeta que
+                lleve en su nombre alguna extension, el programa generar un error.
             """
-            ArchivosParaEstaCarpeta.append(archivo)
+            ArchivosParaEstaCarpeta.append(ruta+archivo)
     """
         el metodo endswith() comprueba que el archivo finaliza con la extension deseada
         ejemplo:
@@ -168,9 +170,17 @@ if __name__ == "__main__":
         ejecutara.
     """
 
-    ruta = "./" # en este campo podemos especificar la ruta en la que trabaja el script
-    # en este caso, la ruta es ./ que es el directorio actual.
+    parse = ArgumentParser() # Realizamos una instancia de la clase ArgumentParser
+    
+    # creamos parametros para nuestro script, si no se usan, usaremos datos por defectos:
+    parse.add_argument("-rf", "--directoryFile", help="se le indica la ruta donde se encuentran los archivos que queremos organizar.", type=str, default="./")
+    parse.add_argument("-ro", "--directoryOutput", help="Se le indica la ruta donde se creara las carpetas y se ordenara los elementos en ellas.", type=str, default="./")
 
+    # parseamos los parametros del programa para poder usarlos
+    parse = parse.parse_args()
+    # Los valores por defecto son la ruta "./", es decir, el directorio actual.
+    
+    
     # Aqui hacemos una instancia a la clase COLOR para poder usar colores en nuestros print:
     _COLOR = COLOR()
     BannerAlerta = "{}[{}!{}] {}".format(_COLOR.LIGHTGREEN_EX, _COLOR.LIGHTMAGENTA_EX, _COLOR.LIGHTGREEN_EX, _COLOR.LIGHTWHITE_EX)
@@ -197,7 +207,7 @@ if __name__ == "__main__":
 
 
     carpetas = ["documentos", "fotos", "videos", "musica", "otros"]
-    crear_carpetas_si_no_existe(carpetas, ruta=ruta)
+    crear_carpetas_si_no_existe(carpetas, ruta=parse.directoryOutput)
     # crea las carpetas necesarias
     
     extensionesdocumentos = [   
@@ -218,7 +228,7 @@ if __name__ == "__main__":
             valores de tipo int en la variable carpetaNumero.
         """
         
-        ListaDeArchivos = asociar_carpetas_con_extensiones(carpetaNumero, extensionesdocumentos[carpetaNumero], ruta=ruta, ArchivosExcepciones=ArchivosExcepciones) 
+        ListaDeArchivos = asociar_carpetas_con_extensiones(carpetaNumero, extensionesdocumentos[carpetaNumero], ruta=parse.directoryFile, ArchivosExcepciones=ArchivosExcepciones) 
         print("{} Archivos a guardar en la carpeta {}, en total un cantidad de {}:".format(BannerOtros, carpetas[carpetaNumero], len(ListaDeArchivos)))
         print("\n".join(ListaDeArchivos))
         """
@@ -232,10 +242,10 @@ if __name__ == "__main__":
                 existentes que se especificaron
             """
             try:
-                print("{} Moviendo el archivo ({}) a la carpeta ({})".format(BannerInformativo, ruta+archivo, carpetas[carpetaNumero]))
-                move(ruta+archivo, carpetas[carpetaNumero])
+                print("{} Moviendo el archivo ({}) a la carpeta ({})".format(BannerInformativo, archivo, carpetas[carpetaNumero]))
+                move(archivo, parse.directoryOutput+carpetas[carpetaNumero])
             except Error:
-                print("{} El archivo {} \t ya existe en el directorio {}".format(BannerAlerta, ruta+archivo, carpetas[carpetaNumero]))
+                print("{} El archivo {} \t ya existe en el directorio {}".format(BannerAlerta, archivo, carpetas[carpetaNumero]))
             
         
 
